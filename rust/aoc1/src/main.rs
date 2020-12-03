9ff5629c56e6112cca44d9ace0c549a2ba16cbac
Stefan Haubold <sthaubol@microsoft.com>
2020-12-03 10:41:49 +0100

    use cargo fmt to format code

--- rust/aoc1/src/main.rs
+++ rust/aoc1/src/main.rs
@@ -4,57 +4,67 @@ use structopt::StructOpt;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
-  /// The path to the file to read
-  path: String,
+    /// The path to the file to read
+    path: String,
 }
 
 fn main() {
-  let args = Cli::from_args();
-  
-  let numbers = read_and_parse(&args.path);
+    let args = Cli::from_args();
 
-  two_numbers(&numbers);
-  three_numbers(&numbers);
+    let numbers = read_and_parse(&args.path);
+
+    two_numbers(&numbers);
+    three_numbers(&numbers);
 }
 
 fn read_and_parse(path: &str) -> Vec<i32> {
-  let contents = fs::read_to_string(path)
-        .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
 
-  let numbers = contents
+    let numbers = contents
         .split_whitespace()
         .map(|s| s.parse().expect("parse error"))
         .collect();
 
-  numbers
+    numbers
 }
 
 fn two_numbers(numbers: &Vec<i32>) {
-  for number_a in numbers.iter() {
-    for number_b in numbers.iter() {
-      if number_a != number_b {
-        let result = number_a + number_b;
-        if result == 2020 {
-          println!("Found {} and {} with the result of {}", number_a, number_b, number_a * number_b);
-          return;
+    for number_a in numbers.iter() {
+        for number_b in numbers.iter() {
+            if number_a != number_b {
+                let result = number_a + number_b;
+                if result == 2020 {
+                    println!(
+                        "Found {} and {} with the result of {}",
+                        number_a,
+                        number_b,
+                        number_a * number_b
+                    );
+                    return;
+                }
+            }
         }
-      }
     }
-  }
 }
 
 fn three_numbers(numbers: &Vec<i32>) {
-  for number_a in numbers.iter() {
-    for number_b in numbers.iter() {
-      for number_c in numbers.iter() {
-        if number_a != number_b && number_a != number_c && number_b != number_c {
-          let result = number_a + number_b + number_c;
-          if result == 2020 {
-            println!("Found {}, {}, {} with the result of {}", number_a, number_b, number_c, number_a * number_b * number_c);
-            return;
-          }
+    for number_a in numbers.iter() {
+        for number_b in numbers.iter() {
+            for number_c in numbers.iter() {
+                if number_a != number_b && number_a != number_c && number_b != number_c {
+                    let result = number_a + number_b + number_c;
+                    if result == 2020 {
+                        println!(
+                            "Found {}, {}, {} with the result of {}",
+                            number_a,
+                            number_b,
+                            number_c,
+                            number_a * number_b * number_c
+                        );
+                        return;
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
--- rust/aoc2/src/main.rs
+++ rust/aoc2/src/main.rs
@@ -5,96 +5,112 @@ use structopt::StructOpt;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
-  /// The path to the file to read
-  path: String,
+    /// The path to the file to read
+    path: String,
 }
 
 fn main() {
-  let args = Cli::from_args();
-  
-  let lines = read_and_parse(&args.path);
+    let args = Cli::from_args();
 
-  let valid1 = count_valid1(lines.clone());
+    let lines = read_and_parse(&args.path);
 
-  println!("Contains {} valid passwords for rule 1", valid1);
+    let valid1 = count_valid1(lines.clone());
 
-  let valid2 = count_valid2(lines);
-  println!("Contains {} valid passwords for rule 2", valid2);
+    println!("Contains {} valid passwords for rule 1", valid1);
+
+    let valid2 = count_valid2(lines);
+    println!("Contains {} valid passwords for rule 2", valid2);
 }
 
 fn read_and_parse(path: &str) -> Vec<String> {
-  let contents = fs::read_to_string(path)
-        .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
 
-  let lines: Vec<String> = contents
-        .split("\n")
-        .map(ToOwned::to_owned)
-        .collect();
+    let lines: Vec<String> = contents.split("\n").map(ToOwned::to_owned).collect();
 
-  return lines;
+    return lines;
 }
 
 fn count_valid1(lines: Vec<String>) -> u64 {
-  let mut valid = 0;
-  for line in lines.iter() {
-    if is_valid1(line) {
-      valid += 1;
-    } else {
+    let mut valid = 0;
+    for line in lines.iter() {
+        if is_valid1(line) {
+            valid += 1;
+        } else {
+        }
     }
-  }
-  valid
+    valid
 }
 
 fn is_valid1(line: &str) -> bool {
-  let parts: Vec<&str> = line.split(':').collect();
-  let rules: Vec<&str> = parts[0].split_whitespace().collect();
-  let minmax: Vec<usize> = rules[0].split('-').map(|s| s.parse().expect("parse error")).collect();
+    let parts: Vec<&str> = line.split(':').collect();
+    let rules: Vec<&str> = parts[0].split_whitespace().collect();
+    let minmax: Vec<usize> = rules[0]
+        .split('-')
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
 
-  // This produced 130. It also returned positive if there where more letters then given in the range :(
-  // let regexp_str = format!(r#"\b(?:[^{letter}\s]*{letter}){{{range}}}[^{letter}\s]*\b"#, letter=rules[1], range=rules[0].replace("-", ","));
-  // println!("{}", regexp_str);
-  // let regexp = Regex::new(regexp_str.as_str()).unwrap();
+    // This produced 130. It also returned positive if there where more letters then given in the range :(
+    // let regexp_str = format!(r#"\b(?:[^{letter}\s]*{letter}){{{range}}}[^{letter}\s]*\b"#, letter=rules[1], range=rules[0].replace("-", ","));
+    // println!("{}", regexp_str);
+    // let regexp = Regex::new(regexp_str.as_str()).unwrap();
 
-  // regexp.is_match(parts[0].replace(" ","").as_str())
+    // regexp.is_match(parts[0].replace(" ","").as_str())
 
-  let count_letter = parts[1].matches(&rules[1]).count();
+    let count_letter = parts[1].matches(&rules[1]).count();
 
-  if count_letter >= minmax[0] && count_letter <= minmax[1] {
-    true
-  } else {
-    false
-  }
+    if count_letter >= minmax[0] && count_letter <= minmax[1] {
+        true
+    } else {
+        false
+    }
 }
 
 fn count_valid2(lines: Vec<String>) -> u64 {
-  let mut valid = 0;
-  for line in lines.iter() {
-    if is_valid2(line) {
-      valid += 1;
-    } else {
+    let mut valid = 0;
+    for line in lines.iter() {
+        if is_valid2(line) {
+            valid += 1;
+        } else {
+        }
     }
-  }
-  valid
+    valid
 }
 
 fn is_valid2(line: &str) -> bool {
-  let parts: Vec<&str> = line.split(':').collect();
-  let rules: Vec<&str> = parts[0].split_whitespace().collect();
-  let minmax: Vec<usize> = rules[0].split('-').map(|s| s.parse().expect("parse error")).collect();
+    let parts: Vec<&str> = line.split(':').collect();
+    let rules: Vec<&str> = parts[0].split_whitespace().collect();
+    let minmax: Vec<usize> = rules[0]
+        .split('-')
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
 
-  let password = parts[1].replace(" ","");
+    let password = parts[1].replace(" ", "");
 
-  // this would not work with utf8 passwords
-  let letter_a = password.chars().nth(minmax[0]-1).expect("message").to_string();
-  let letter_b = password.chars().nth(minmax[1]-1).expect("message").to_string();
+    // this would not work with utf8 passwords
+    let letter_a = password
+        .chars()
+        .nth(minmax[0] - 1)
+        .expect("message")
+        .to_string();
+    let letter_b = password
+        .chars()
+        .nth(minmax[1] - 1)
+        .expect("message")
+        .to_string();
 
-  if (letter_a == rules[1] || letter_b == rules[1]) && (letter_a != letter_b)  {
-    println!("valid: {} -> {}/{} {}/{}", line, letter_a, rules[1], letter_b, rules[1]);
+    if (letter_a == rules[1] || letter_b == rules[1]) && (letter_a != letter_b) {
+        println!(
+            "valid: {} -> {}/{} {}/{}",
+            line, letter_a, rules[1], letter_b, rules[1]
+        );
 
-    true
-  } else {
-    println!("invalid: {} -> {}/{} {}/{}", line, letter_a, rules[1], letter_b, rules[1]);
+        true
+    } else {
+        println!(
+            "invalid: {} -> {}/{} {}/{}",
+            line, letter_a, rules[1], letter_b, rules[1]
+        );
 
-    false
-  }
+        false
+    }
 }
