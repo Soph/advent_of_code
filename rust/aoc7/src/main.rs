0f3e1331955d6197d2f38ec0381a0e5ca2a6a4c8
Stefan Haubold <sthaubol@microsoft.com>
2020-12-07 12:52:37 +0100

    applied cargo fmt

--- rust/aoc7/src/main.rs
+++ rust/aoc7/src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
-use structopt::StructOpt;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::fs;
+use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -29,7 +29,6 @@ fn main() {
         println!("");
     }
 
-
     let mut valid_bags: HashSet<String> = HashSet::new();
     bags_contained_count(&all_bags, &"shiny gold".to_string(), &mut valid_bags);
 
@@ -38,12 +37,16 @@ fn main() {
     println!("Count: {}", &valid_bags.len());
 }
 
-fn bags_contained_count(bags: &HashMap<String, HashSet<String>>, name: &String, result: &mut HashSet<String>) {
+fn bags_contained_count(
+    bags: &HashMap<String, HashSet<String>>,
+    name: &String,
+    result: &mut HashSet<String>,
+) {
     match bags.get(name) {
         None => {
             println!("{} is not allowed in any other bag", name);
             result.insert(name.clone());
-        },
+        }
         Some(bag_bags) => {
             result.insert(name.clone());
             for bag in bag_bags {
@@ -62,8 +65,11 @@ fn read_and_parse(path: &str, main_bag: &String) -> HashMap<String, HashSet<Stri
         let bag = parse_bag(&row);
         if bag.name != *main_bag {
             for name in bag.bags {
-                bag_mapping.entry(name).or_insert(HashSet::new()).insert(bag.name.clone());
-            }            
+                bag_mapping
+                    .entry(name)
+                    .or_insert(HashSet::new())
+                    .insert(bag.name.clone());
+            }
         }
     }
 
@@ -74,19 +80,28 @@ fn parse_bag(data: &str) -> Bag {
     let parts: Vec<String> = data.split(" contain ").map(ToOwned::to_owned).collect();
     let bag_name = normalize_bag_name(&parts[0]);
     if parts[1] == "no other bag" {
-        return Bag { name: bag_name.clone(), bags: vec![] };
+        return Bag {
+            name: bag_name.clone(),
+            bags: vec![],
+        };
     }
 
-    let contains: Vec<String> = parts[1].replace(".", "").split(", ").map(|s| normalize_bag_name(&s[2..s.len()].to_string())).collect();
+    let contains: Vec<String> = parts[1]
+        .replace(".", "")
+        .split(", ")
+        .map(|s| normalize_bag_name(&s[2..s.len()].to_string()))
+        .collect();
 
     println!("{} contains {}", bag_name, contains.join(","));
-    Bag { name: bag_name.clone(), bags: contains }
+    Bag {
+        name: bag_name.clone(),
+        bags: contains,
+    }
 }
 
 fn normalize_bag_name(name: &String) -> String {
     name.replace(" bags", "").replace(" bag", "")
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -94,9 +109,9 @@ mod tests {
 
     #[test]
     fn test_parsing() {
-       let bag = parse_bag(&"muted white bags contain 4 dark orange bags, 3 bright white bags.");
-       assert_eq!(bag.name, "muted white");
-       assert_eq!(bag.bags[0], "dark orange");
-       assert_eq!(bag.bags[1], "bright white");
+        let bag = parse_bag(&"muted white bags contain 4 dark orange bags, 3 bright white bags.");
+        assert_eq!(bag.name, "muted white");
+        assert_eq!(bag.bags[0], "dark orange");
+        assert_eq!(bag.bags[1], "bright white");
     }
 }
