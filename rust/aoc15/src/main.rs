b2e77176a149b4f79baf336d1d32a622de366099
Stefan Haubold <sthaubol@microsoft.com>
2020-12-15 11:14:15 +0100

    optimized day15

--- rust/aoc15/src/main.rs
+++ rust/aoc15/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     let result1 = run(&data, 2020);
     println!("Result for 2020th - {}", result1);
 
-    let result2 = run(&data, 30000000);
+    let result2 = run2(&data, 30000000);
     println!("Result for 30000000th - {}", result2);
 }
 
@@ -76,6 +76,41 @@ fn run(data: &Vec<u64>, nth: u64) -> u64 {
             None => {
                 println!("Not in History, should not happen!?");
             }
+        }
+    }
+}
+
+// after takling about the solution I realized I don't need to store two last values, one is enough
+fn run2(data: &Vec<u64>, nth: u64) -> u64 {
+    let mut used: HashMap<u64, u64> = HashMap::new();
+
+    for i in 0..data.len() - 1 {
+        used.insert(data[i], i as u64 + 1);
+    }
+    let mut current_value = *data.last().unwrap();
+    let mut i = data.len() as u64;
+
+    loop {
+        let last_iteration = used.get_mut(&current_value);
+
+        match last_iteration {
+            Some(x) => {
+                // println!("{}: Found for {} , calculating: {} - {}", i, current_value, i, x);
+                // Spoken before
+                current_value = i - *x;
+                *x = i; // update hashed value
+            }
+            None => {
+                // println!("{}: Not Found, set 0 for {}", i, current_value);
+                // Never spoken before
+                used.insert(current_value, i);
+                current_value = 0;
+            }
+        }
+        i += 1;
+
+        if i == nth {
+            return current_value;
         }
     }
 }
