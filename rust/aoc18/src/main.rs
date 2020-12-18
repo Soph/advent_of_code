76c4c731ca8a6e2ca4fb86394f8c3b841f3311a5
Stefan Haubold <sthaubol@microsoft.com>
2020-12-18 15:30:04 +0100

    run cargo fmt

--- rust/aoc18/src/main.rs
+++ rust/aoc18/src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
-use structopt::StructOpt;
 use regex::Regex;
 use std::collections::HashMap;
+use std::fs;
+use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -29,19 +29,15 @@ fn main() {
     }
 
     println!("Result2: {}", sum)
-
 }
 
 fn read_and_parse(path: &String) -> Vec<String> {
     let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
 
-    contents
-        .split("\n")
-        .map(ToOwned::to_owned)
-        .collect()
+    contents.split("\n").map(ToOwned::to_owned).collect()
 }
 
-fn calculate_all(line: &String, calculate_numbers:fn (&String) -> u64) -> u64 {
+fn calculate_all(line: &String, calculate_numbers: fn(&String) -> u64) -> u64 {
     let mut processing = line.clone();
     let regex = Regex::new(r"\([^\)\(]*\)").unwrap();
     loop {
@@ -49,7 +45,10 @@ fn calculate_all(line: &String, calculate_numbers:fn (&String) -> u64) -> u64 {
         let mut replacements: HashMap<String, u64> = HashMap::new();
         for capture in regex.captures_iter(processing.as_str()) {
             found = true;
-            replacements.insert(capture[0].to_string(), calculate_numbers(&capture[0].replace("(","").replace(")","").to_string()));
+            replacements.insert(
+                capture[0].to_string(),
+                calculate_numbers(&capture[0].replace("(", "").replace(")", "").to_string()),
+            );
         }
 
         for (key, value) in replacements {
@@ -62,11 +61,10 @@ fn calculate_all(line: &String, calculate_numbers:fn (&String) -> u64) -> u64 {
             return result;
         }
     }
-
 }
 
 fn calculate_numbers1(numbers: &String) -> u64 {
-    let mut processing = numbers.replace(" ","");
+    let mut processing = numbers.replace(" ", "");
     let regex = Regex::new(r"(\d+)([\+\*])(\d+)").unwrap();
 
     loop {
@@ -74,18 +72,22 @@ fn calculate_numbers1(numbers: &String) -> u64 {
         let mut replacements: HashMap<String, u64> = HashMap::new();
         let captures = regex.captures_iter(processing.as_str()).next();
         match captures {
-            Some(capture) => {
-                match &capture[2] {
-                    "+" => {
-                        found = true;
-                        replacements.insert(capture[0].to_string(), capture[1].parse::<u64>().unwrap() + capture[3].parse::<u64>().unwrap());
-                    },
-                    "*" => {
-                        found = true;
-                        replacements.insert(capture[0].to_string(), capture[1].parse::<u64>().unwrap() * capture[3].parse::<u64>().unwrap());
-                    },
-                    _ => (),
-                }  
+            Some(capture) => match &capture[2] {
+                "+" => {
+                    found = true;
+                    replacements.insert(
+                        capture[0].to_string(),
+                        capture[1].parse::<u64>().unwrap() + capture[3].parse::<u64>().unwrap(),
+                    );
+                }
+                "*" => {
+                    found = true;
+                    replacements.insert(
+                        capture[0].to_string(),
+                        capture[1].parse::<u64>().unwrap() * capture[3].parse::<u64>().unwrap(),
+                    );
+                }
+                _ => (),
             },
             None => (),
         }
@@ -100,7 +102,7 @@ fn calculate_numbers1(numbers: &String) -> u64 {
 }
 
 fn calculate_numbers2(numbers: &String) -> u64 {
-    let mut processing = numbers.replace(" ","");
+    let mut processing = numbers.replace(" ", "");
     let regex_add = Regex::new(r"(\d+)([\+])(\d+)").unwrap();
     let regex_mul = Regex::new(r"(\d+)([\*])(\d+)").unwrap();
 
@@ -109,14 +111,15 @@ fn calculate_numbers2(numbers: &String) -> u64 {
         let mut replacements: HashMap<String, u64> = HashMap::new();
         let captures = regex_add.captures_iter(processing.as_str()).next();
         match captures {
-            Some(capture) => {
-                match &capture[2] {
-                    "+" => {
-                        found = true;
-                        replacements.insert(capture[0].to_string(), capture[1].parse::<u64>().unwrap() + capture[3].parse::<u64>().unwrap());
-                    },
-                    _ => (),
-                }  
+            Some(capture) => match &capture[2] {
+                "+" => {
+                    found = true;
+                    replacements.insert(
+                        capture[0].to_string(),
+                        capture[1].parse::<u64>().unwrap() + capture[3].parse::<u64>().unwrap(),
+                    );
+                }
+                _ => (),
             },
             None => (),
         }
@@ -128,20 +131,21 @@ fn calculate_numbers2(numbers: &String) -> u64 {
             break;
         }
     }
-    
+
     loop {
         let mut found = false;
         let mut replacements: HashMap<String, u64> = HashMap::new();
         let captures = regex_mul.captures_iter(processing.as_str()).next();
         match captures {
-            Some(capture) => {
-                match &capture[2] {
-                    "*" => {
-                        found = true;
-                        replacements.insert(capture[0].to_string(), capture[1].parse::<u64>().unwrap() * capture[3].parse::<u64>().unwrap());
-                    },
-                    _ => (),
-                }  
+            Some(capture) => match &capture[2] {
+                "*" => {
+                    found = true;
+                    replacements.insert(
+                        capture[0].to_string(),
+                        capture[1].parse::<u64>().unwrap() * capture[3].parse::<u64>().unwrap(),
+                    );
+                }
+                _ => (),
             },
             None => (),
         }
@@ -168,24 +172,49 @@ mod tests {
 
     #[test]
     fn test_calculate2() {
-        assert_eq!(calculate_numbers2(&"1 + 2 * 3 + 4 * 5 + 6".to_string()), 231);
+        assert_eq!(
+            calculate_numbers2(&"1 + 2 * 3 + 4 * 5 + 6".to_string()),
+            231
+        );
         assert_eq!(calculate_numbers2(&"4 * 11".to_string()), 44);
         assert_eq!(calculate_numbers2(&"3 * 2 * 3".to_string()), 18);
     }
 
-
     #[test]
     fn test_calculate_all() {
-        assert_eq!(calculate_all(&"7 * 7 * (5 + 9 + 9) * (8 * 6 * 5 + 4 * 2)".to_string(), calculate_numbers1), 549976);
+        assert_eq!(
+            calculate_all(
+                &"7 * 7 * (5 + 9 + 9) * (8 * 6 * 5 + 4 * 2)".to_string(),
+                calculate_numbers1
+            ),
+            549976
+        );
         assert_eq!(calculate_all(&"((5 + 8 + 7 + 6 * 6 + 3) + 4 * 3 * (5 * 2) * 5 * (2 * 9)) + 6 + 3 * (2 * (2 + 4 + 8) * 4) + (9 + 3 + 6 * (9 + 5 * 6 * 3 * 8 * 5) + (4 * 6) + (6 * 3 + 2 + 8))".to_string(), calculate_numbers1), 49473700);
-        assert_eq!(calculate_all(&"4 + 5 + 6 * 6 * 6 + (3 * 2 * 3 * (2 * 6 + 9) + 6 + (5 + 2 + 9 * 6))".to_string(), calculate_numbers1), 1020);
+        assert_eq!(
+            calculate_all(
+                &"4 + 5 + 6 * 6 * 6 + (3 * 2 * 3 * (2 * 6 + 9) + 6 + (5 + 2 + 9 * 6))".to_string(),
+                calculate_numbers1
+            ),
+            1020
+        );
     }
 
     #[test]
     fn test_calculate_all2() {
-        assert_eq!(calculate_all(&"7 * 7 * (5 + 9 + 9) * (8 * 6 * 5 + 4 * 2)".to_string(), calculate_numbers2), 549976);
+        assert_eq!(
+            calculate_all(
+                &"7 * 7 * (5 + 9 + 9) * (8 * 6 * 5 + 4 * 2)".to_string(),
+                calculate_numbers2
+            ),
+            549976
+        );
         assert_eq!(calculate_all(&"((5 + 8 + 7 + 6 * 6 + 3) + 4 * 3 * (5 * 2) * 5 * (2 * 9)) + 6 + 3 * (2 * (2 + 4 + 8) * 4) + (9 + 3 + 6 * (9 + 5 * 6 * 3 * 8 * 5) + (4 * 6) + (6 * 3 + 2 + 8))".to_string(), calculate_numbers2), 49473700);
-        assert_eq!(calculate_all(&"4 + 5 + 6 * 6 * 6 + (3 * 2 * 3 * (2 * 6 + 9) + 6 + (5 + 2 + 9 * 6))".to_string(), calculate_numbers2), 1020);
+        assert_eq!(
+            calculate_all(
+                &"4 + 5 + 6 * 6 * 6 + (3 * 2 * 3 * (2 * 6 + 9) + 6 + (5 + 2 + 9 * 6))".to_string(),
+                calculate_numbers2
+            ),
+            1020
+        );
     }
-
 }
