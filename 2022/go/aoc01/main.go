c8bdda53c4a1672034aebdcbb2789fffc350a445
Stefan Haubold <soph@MacBook-Pro-von-Stefan.local>
2022-12-01 18:01:05 +0100

    go fmt

--- 2022/go/aoc01/main.go
+++ 2022/go/aoc01/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -10,17 +10,17 @@ import (
 
 func main() {
 	fileName := os.Args[1]
-    dat, err := os.ReadFile(fileName)
+	dat, err := os.ReadFile(fileName)
 	if err != nil {
-        panic(err)
-    }
+		panic(err)
+	}
 
-	elf_strings := strings.Split(string(dat), "\n\n");
+	elf_strings := strings.Split(string(dat), "\n\n")
 
 	var elfs []int
 	for _, elf_string := range elf_strings {
 		elf := 0
-		strings := strings.Split(string(elf_string), "\n");
+		strings := strings.Split(string(elf_string), "\n")
 		for _, str := range strings {
 			number, err := strconv.Atoi(str)
 			if err != nil {
@@ -34,7 +34,7 @@ func main() {
 	fmt.Printf("Part1: %d\n", elfs[len(elfs)-1])
 
 	sum := 0
-	for _,value := range elfs[len(elfs)-4:len(elfs)-1] {
+	for _, value := range elfs[len(elfs)-4 : len(elfs)-1] {
 		sum += value
 	}
 	fmt.Printf("Part2: %d\n", sum)
