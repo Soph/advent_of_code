fce85c36a24822b95105f43a9ad6bf2bc3c2c8af
Stefan Haubold <soph@github.com>
2022-12-19 18:17:11 +0100

    :facepalm:

--- 2022/ruby/aoc19/aoc.rb
+++ 2022/ruby/aoc19/aoc.rb
@@ -1,11 +1,13 @@
 require "set"
 raw_blueprints = File.open(ARGV[0])
+part = ARGV[1]
 
-blueprints = {}
+blueprints = Set.new
 raw_blueprints.each do |raw_blueprint|
   data = raw_blueprint.match(/Blueprint (\d*): Each ore robot costs (\d*) ore. Each clay robot costs (\d*) ore. Each obsidian robot costs (\d*) ore and (\d*) clay. Each geode robot costs (\d*) ore and (\d*) obsidian./)
 
-  blueprints[data[1].to_i] = {
+  blueprints << {
+    id: data[1].to_i,
     ore_robot: [data[2].to_i],
     clay_robot: [data[3].to_i],
     obsidian_robot: [data[4].to_i, data[5].to_i],
@@ -21,7 +23,8 @@ def update_produce(variant, time)
 end
 
 max_geode = {}
-blueprints.each do |id, blueprint|
+blueprints = blueprints.to_a[0..2] if part == "2"
+blueprints.each do |blueprint|
   options = Set[{
     ore: 0,
     ore_robot: 1,
@@ -31,106 +34,122 @@ blueprints.each do |id, blueprint|
     obsidian_robot: 0,
     geode: 0,
     geode_robot: 0,
-    time: 24
+    time: part == "2" ? 32 : 24
   }]
-  puts blueprint.inspect
   done = Set.new
   max = 0
+  i = 0
+  cache = {}
   loop do
-    #puts options.inspect
-    #sleep 1
     option = options.take(1).first
-    break if options.size == 0
+    break if option.nil?
     options.delete(option)
 
-    if (option[:geode] + (option[:geode_robot] + 1) * option[:time]) <= max
-      #throw away
+    if max > 0 && (option[:geode] + option[:time] * option[:geode_robot] + option[:time] * (option[:time] - 1) / 2) < max
       next
-    end
+    end    
 
     new_options = Set.new
     if option[:obsidian_robot] > 0 # produces obsidian -> can build geode robot now
       ore_needed = [0,(blueprint[:geode_robot][0] - option[:ore])].max
       obsidian_needed = [0,(blueprint[:geode_robot][1] - option[:obsidian])].max
-      time = [(ore_needed.to_f/option[:ore_robot]).ceil, (obsidian_needed.to_f/option[:obsidian_robot]).ceil].max
-      #puts "Geode: ore: #{blueprint[:geode_robot][0]}/#{ore_needed}, obsidian: #{blueprint[:geode_robot][1]}/#{obsidian_needed} -> time: #{time}"
-      time += 1 # until ready
-      if option[:time] - time > 0 # enough time
+      time = 1 # robot build time
+      if ore_needed != 0 || obsidian_needed != 0
+        time += [(ore_needed.to_f/option[:ore_robot]).ceil, (obsidian_needed.to_f/option[:obsidian_robot]).ceil].max
+      end
+      if option[:time] - time >= 1 # enough time
         opt = option.clone
         opt[:time] -= time
         update_produce(opt, time)
         opt[:geode_robot] += 1
         opt[:ore] -= blueprint[:geode_robot][0] # exising ore + new produced ore - cost of ore for robot
         opt[:obsidian] -= blueprint[:geode_robot][1] # exising ore + new produced ore - cost of ore for robot
-        new_options << opt
-        puts "Build Geode Robot. time:#{time} - #{option} -> #{opt}"
+        unless done.include?(opt)
+          new_options << opt
+        end
       end
     end
 
     if option[:clay_robot] > 0 # produces clay -> can build obsidian robot now
       ore_needed = [0,(blueprint[:obsidian_robot][0] - option[:ore])].max
       clay_needed = [0,(blueprint[:obsidian_robot][1] - option[:clay])].max
-      time = 1
+      time = 1 # robot build time
       if ore_needed != 0 || clay_needed != 0
         time += [(ore_needed.to_f/option[:ore_robot]).ceil, (clay_needed.to_f/option[:clay_robot]).ceil].max
       end
-      #puts "Obsidian: ore: #{blueprint[:obsidian_robot][0]}/#{ore_needed}, obsidian: #{blueprint[:obsidian_robot][1]}/#{clay_needed} -> time: #{time}"
 
-      if option[:time] - time > 0 # enough time
+      if option[:time] - time >= 3 # at least time to produce an obsidian, build a geode bot, and let it produce one thing
         opt = option.clone
         opt[:time] -= time
         update_produce(opt, time)
         opt[:obsidian_robot] += 1
         opt[:ore] -= blueprint[:obsidian_robot][0] # exising ore + new produced ore - cost of ore for robot
         opt[:clay] -= blueprint[:obsidian_robot][1] # exising ore + new produced ore - cost of ore for robot
-        new_options << opt
+        unless done.include?(opt)
+          new_options << opt
+        end
       end
     end
     # build another clay robot
-    time = ([blueprint[:clay_robot][0] - option[:ore], 0].max.to_f/option[:ore_robot]).ceil
-    time += 1 # until ready
-    if option[:time] - time > 0 # enough time
+    ore_needed = [blueprint[:clay_robot][0] - option[:ore], 0].max
+    time = 1 # bot build time
+    if ore_needed > 0 
+      time += (ore_needed.to_f/option[:ore_robot]).ceil
+    end
+    if option[:time] - time >= 5 # produce a clay, build a obsidian bot, let it produce an obsidian, build a geode bot and let it produce one thing
       opt = option.clone
       opt[:time] -= time
       update_produce(opt, time)
       opt[:clay_robot] += 1
       opt[:ore] -= blueprint[:clay_robot][0] # exising ore + new produced ore - cost of ore for robot
-
-      new_options << opt
+      unless done.include?(opt)
+        new_options << opt
+      end
     end
     # build another ore robot
-    time = ([blueprint[:ore_robot][0] - option[:ore],0].max.to_f/option[:ore_robot]).ceil
-    time += 1 # until ready
-    if option[:time] - time > 0 # enough time
+    ore_needed = [blueprint[:ore_robot][0] - option[:ore], 0].max
+    time = 1 # bot build time
+    if ore_needed > 0 
+      time += (ore_needed.to_f/option[:ore_robot]).ceil
+    end
+    if option[:time] - time >= 3 # at least time to produce an ore, build a geode bot, let it produce one geode
       opt = option.clone
       opt[:time] -= time
       update_produce(opt, time)
       opt[:ore_robot] += 1
       opt[:ore] -= blueprint[:ore_robot][0] # exising ore + new produced ore - cost of ore for robot
-      new_options << opt
+      unless done.include?(opt)
+        new_options << opt
+      end
     end
 
     ## done building
-    found = false
-    new_options.each do |new_option|
-      next if done.include?(new_option)
-      next if options.include?(new_option)
-      options << new_option
-      found = true
+    if new_options.empty?
+      done << option
+    else
+      new_max = new_options.map{|d| d[:geode] + (d[:geode_robot] * d[:time])}.max
+      if new_max && new_max > max
+        max = new_max
+      end
+      new_options.each do |option|        
+        cache_key = option.values[0..-2]
+        if cache[cache_key] && cache[cache_key] < option[:time]
+          next
+        end
+        cache[cache_key] = option[:time]
+        options << option
+      end
     end
-    done << option if !found
-
-    new_max = done.map{|d| d[:geode] + (d[:geode_robot] * d[:time])}.max
-    if new_max && new_max > max
-      max = new_max
-    end
-   
-    puts "done: #{done.size} options: #{options.size} max:#{max}"
+    i += 1
+    puts "#{blueprint[:id]} done: #{done.size} options: #{options.size} max:#{max}" if i % 10000 == 0
   end
-  #puts done.sort_by{|d| d[:geode_robot]}
-  #sleep 20
-  max_geode[id] = (done + options).map{|d| d[:geode] + (d[:geode_robot] * d[:time])}.max
-  puts "Next Blueprint"
-  sleep 1
+  done += options
+  puts done.sort_by{|d| d[:geode] + (d[:geode_robot] * d[:time])}.last
+  max_geode[blueprint[:id]] = done.map{|d| d[:geode] + (d[:geode_robot] * d[:time])}.max
+  puts "Blueprint #{blueprint[:id]} done"
 end
-puts max_geode.inspect
+if part == "1"
+  puts "Result 1 (Quality Levels): #{max_geode.map{|key,value| key * value}.sum}"
+else
+  puts "Result 2: #{max_geode.values.inject(:*)}"
+end
