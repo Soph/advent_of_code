d8713cd1ff3ca4d1c03e6db005626ce7fa2abd11
Stefan Haubold <soph@github.com>
2023-12-12 22:02:14 +0100

    2023-12-12 ruby part 2 /shrug

--- 2023/ruby/aoc12/aoc.rb
+++ 2023/ruby/aoc12/aoc.rb
@@ -2,30 +2,57 @@ file = File.open(ARGV[0])
 lines = file.readlines.map(&:chomp)
 
 records = lines.map {|line| line.split(" ") }
-records = records.map {|record| [record[0], record[1].split(",").map(&:to_i)]}
+records = records.map {|record| [record[0].chars, record[1].split(",").map(&:to_i)]}
 
-puts records.inspect
+CACHE = {}
+def check(record, groups)
+  result = CACHE[[record.size, groups.size]]
+  return result if result
 
-sum = 0
-records.each_with_index do |record, i|
-  puts i
-  variants = [record[0]]
-  while true do
-    new_variants = []
-    found = false
-    variants.each do |variant|
-      found = true if variant =~ /\?/
-      new_variants << variant.sub("?", '#')
-      new_variants << variant.sub("?", '.')
-    end
-    break unless found
-    
-    variants = new_variants.uniq
+  return 0 if record.size < (groups.size - 1 + groups.sum)
+  
+  if groups.empty? && record.select{|c| c == '#'}.empty?
+    return 1
   end
-  matches = variants.select{|variant| variant.chars.chunk{|y| y}.map{|y, ys| [y, ys.length]}.select{|k,v| k == '#'}.map{|k| k[1]} == record[1] }
-  #puts "Record: #{record[0]} - #{record[1]}"
-  #puts matches.inspect
-  sum += variants.select{|variant| variant.chars.chunk{|y| y}.map{|y, ys| [y, ys.length]}.select{|k,v| k == '#'}.map{|k| k[1]} == record[1] }.size
+
+  if groups.empty? || record.nil?
+    return 0
+  end
+
+  if record[0] == '.'
+    return check(record[1..], groups)
+  end
+
+  if record[0] == '?'
+    a = check(record[1..].insert(0,'#'), groups)
+    b = check(record[1..].insert(0,'.'), groups)
+    CACHE[[record.size, groups.size]] = a+b
+    return a+b
+  end
+
+  if record[0] == '#'
+    group = groups[0]
+    if record[0...group].select {|c| c == '.'}.empty? # matches
+      if record[group] == '#' # not possible        
+        return 0
+      end
+
+      new_record = record[(group+1)..]
+      new_record ||= []
+      return check(new_record, groups[1..])
+    else
+      return 0
+    end
+  end
+
+  0
 end
 
-puts "Part1: #{sum}"
+sum = 0
+records.each do |record|
+  CACHE = {}
+  new_record = ([record[0].join]*5).join("?").chars
+  new_groups = ([record[1].join(",")]*5).join(",").split(",").map(&:to_i)
+  sum += check(new_record, new_groups)
+end
+puts sum
