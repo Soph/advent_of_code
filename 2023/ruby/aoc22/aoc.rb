1d2b4de2b4d3df36bd52934522caaaf4e2833ddb
Stefan Haubold <soph@github.com>
2023-12-22 13:50:03 +0100

    2023-12-22 ruby part 2

--- 2023/ruby/aoc22/aoc.rb
+++ 2023/ruby/aoc22/aoc.rb
@@ -1,3 +1,4 @@
+require "pry"
 require "Set"
 
 file = File.open(ARGV[0])
@@ -5,7 +6,7 @@ lines = file.readlines.map(&:chomp)
 
 cubes = lines.map{|line| line.split("~").map{|part| part.split(",").map(&:to_i)}}
 
-puts cubes.inspect
+#puts cubes.inspect
 
 cubes = cubes.sort_by{|cube| cube[0][2] }
 
@@ -40,7 +41,7 @@ end
 
 # brick supports [bricks]
 supported = {}
-puts occupied.inspect
+#puts occupied.inspect
 
 cubes.each_with_index do |cube, i|
   (cube[0][1]..cube[1][1]).each do |y|
@@ -54,8 +55,8 @@ cubes.each_with_index do |cube, i|
   end
 end
 
-puts
-puts supported.inspect
+#puts
+#puts supported.inspect
 
 need_to_stay = Set.new
 cubes.each_index do |i|
@@ -63,5 +64,25 @@ cubes.each_index do |i|
     need_to_stay << supported[i]
   end
 end
-puts need_to_stay.inspect
-puts cubes.size - need_to_stay.size
+#puts need_to_stay.inspect
+puts "Part 1: #{cubes.size - need_to_stay.size}"
+
+count = 0
+cubes.each_with_index do |cube, cube_id|
+  to_check = [cube_id]
+  falling = Set.new
+  falling << cube_id
+  while !to_check.empty?
+    check = to_check.shift
+    supported.each do |cube_above, supports|
+      next if cube_above == cube_id || falling.include?(cube_above)
+      if supports.size > 0 && (supports.to_a - falling.to_a).size == 0
+        to_check << cube_above
+        falling << cube_above
+      end
+    end
+  end
+  count += falling.size - 1 if falling.size > 0
+  puts "#{cube_id}" if cube_id % 20 == 0
+end
+puts "Part 2: #{count}"
