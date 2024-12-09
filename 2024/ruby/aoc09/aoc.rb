1fcd04be820b33c09bbde8abd43e3b320e060a06
Stefan Haubold <soph@github.com>
2024-12-09 10:43:18 +0100

    working ruby day 9

--- 2024/ruby/aoc09/aoc.rb
+++ 2024/ruby/aoc09/aoc.rb
@@ -40,104 +40,39 @@ end
 puts "Part1: #{sum}"
 
 p2_memory = memory.map(&:clone)
-puts p2_memory[0..100].flatten.join.inspect
-
 groups.reverse.each do |group|
-  #puts "Group: #{group}"
-  p2_memory.each_slice(2).with_index do |result, index|
-    data = result[0]
-    free = result[1]
-    next if free.size < group.size
-    #puts "#{data} #{free} #{index}"
+  group_index = p2_memory.index(group)
+  first_index = p2_memory.index { |data| data[0] == "." && data.size >= group.size }
 
-    source_index = p2_memory.index(group)
-    #puts source_index
-    next if source_index.nil?
-    # replace source with free
-    p2_memory[source_index] = []
-    p2_memory[source_index] += ["."] * group.size
+  # is this more forward?
+  next if first_index.nil? || group_index <= first_index
 
-    p2_memory[index*2+1] = []
-    p2_memory.insert(index*2+2, group)
-    p2_memory.insert(index*2+3, ["."] * (free.size - group.size))
-    #puts p2_memory[0..100].flatten.join.inspect
-    break
+  # remove old
+  before_old = p2_memory[group_index-1]
+  after_old = p2_memory[group_index+1]
+  if before_old[0] == "." && after_old[0] == "."
+    p2_memory.delete_at(group_index+1)
+    p2_memory.delete_at(group_index)
+    p2_memory.delete_at(group_index-1)
+    p2_memory.insert(group_index-1, ["."]*(group.size+before_old.size+after_old.size))
+  elsif before_old[0] == "."
+    p2_memory.delete_at(group_index)
+    p2_memory.delete_at(group_index-1)
+    p2_memory.insert(group_index-1, ["."]*(group.size+before_old.size))
+  elsif after_old[0] == "."
+    p2_memory.delete_at(group_index+1)
+    p2_memory.delete_at(group_index)
+    p2_memory.insert(group_index, ["."]*(group.size+after_old.size))
+  else
+    p2_memory[group_index] = ["."]*group.size
   end
+  
+  p2_memory[first_index] = ["."] * (p2_memory[first_index].size - group.size)
+  p2_memory.insert(first_index, group)
 end
 
-#puts p2_memory.flatten.join.inspect
 sum = 0
 p2_memory.flatten.each_with_index do |file_id, index|
   sum += file_id.to_i * index
 end
 puts "Part2: #{sum}"
-
-
-# puts memory_2.inspect
-# (memory_2.size-1).downto(0).each do |index|
-#   from_memory = memory_2_org[index]
-#   #puts from_memory.inspect
-#   memory_2[0..index].each_with_index do |data, data_index|
-#     from_data_size = from_memory[0].size
-#     to_free_size = data[1].size
-#     next if to_free_size < from_data_size
-
-#     # copy data to new position
-#     data[0] += from_memory[0]
-#     # remove free space at new position
-#     data[1] = ["."] * (to_free_size - from_data_size)
-
-#     # update old position
-#     #puts "#{from_memory[1].size} + #{from_data_size}"
-#     memory_2[index] = [memory_2[index][0]-from_memory[0], memory_2[index][1]]
-#     memory_2.insert(index, [[], ["."] * from_data_size])
-#     #puts memory_2.flatten.join.inspect
-#     break
-#   end
-# end
-
-# puts memory_2.flatten.join.inspect
-# sum = 0
-# memory_2.flatten.join.chars.each_with_index do |file_id, index|
-#   sum += file_id.to_i * index
-# end
-# puts "Part1: #{sum}"
-
-# # files_2 = files.clone.flatten
-# # new_memory = []
-# # spaces.each_with_index do |free, index|
-# #   free.times do |i|
-# #     files[index] << files_2.pop
-# #   end
-# # end
-
-# # puts files.inspect
-
-
-# # diskmap = lines[0].chars
-# # diskmap_new = []
-# # max_file_id = diskmap.size / 2
-
-# # diskmap.each_slice(2) do |pos, index|
-# #   if index % 2 == 0
-# #     diskmap_new << ((index / 2).to_s * pos.to_i).chars
-# #   else
-# #     while count > 0
-# #       if diskmap[max_file_id * 2].to_i > count
-# #         diskmap_new 
-
-# #     count = pos.to_i
-# #     if diskmap[max_file_id * 2].to_i > count
-
-# #     else
-
-# #     end
-# #     while count > 0
-# #       diskmap[end_index] 
-# #     end
-# #     #new_end_index = end_index - pos.to_i
-# #     #diskmap << diskmap[new_end_index, end_index]
-# #   end
-# # end
-
-
