dec89a2ad00e8462bc576608a9be08090cd41ed2
Stefan Haubold <stefan@haubi.com>
2025-12-01 09:40:11 +0100

    aoc 2025 01, part1 working

---
+++ 2025/ruby/aoc01/aoc.rb
@@ -0,0 +1,41 @@
+file = File.open(ARGV[0])
+lines = file.readlines.map(&:chomp)
+
+dial = 50
+zeros = 0
+
+lines.each do |line|
+  if line[0] == 'R'
+    dial += line[1..].to_i
+  else
+    dial -= line[1..].to_i
+  end
+  dial %= 100
+  zeros += 1 if dial.zero?
+end
+
+puts "Part 1: #{zeros}"
+
+dial = 50
+zeros = 0
+
+lines.each do |line|
+  mov = line[1..].to_i
+  zeros += mov / 100
+  before = dial
+  if line[0] == 'R'
+    dial += mov
+  else
+    dial -= mov
+  end
+  before_mod = dial
+  dial %= 100
+  if dial.zero?
+    zeros += 1
+  elsif before != 0 && before_mod != dial
+    zeros += 1
+  end
+  puts "#{before} #{line[0] == 'R' ? '+' : '-'} #{line[1..]} -> #{dial} --> #{zeros}" # if zeros - before_zeros > 0
+end
+
+puts "Part 2: #{zeros}"
