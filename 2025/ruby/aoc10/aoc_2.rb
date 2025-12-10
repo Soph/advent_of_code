bbc36063ea23b9086a5be44643932831a37921b4
Stefan Haubold <stefan@haubi.com>
2025-12-10 18:10:15 +0100

    not working either, but let's keep that version

--- 2025/ruby/aoc10/aoc_2.rb
+++ 2025/ruby/aoc10/aoc_2.rb
@@ -6,32 +6,68 @@ data = file.readlines.map(&:chomp).map do |l|
     b[1..-2].split(',').map(&:to_i)
   end
   joltage = parts[-1][1..-2].split(',').map(&:to_i)
-  joltage = Hash[(0...joltage.size).zip joltage]
   [target, buttons, joltage, l]
 end
 
-# puts data.inspect
+def prefill(data, result, count, size)
+  removed = Set.new
+  values = Set.new
+  values << Array.new(size, 0)
+  count.times do
+    new_values = Set.new
+    values.each do |v|
+      data.each do |d|
+        new_v = v.dup
+        d.each do |n|
+          new_v[n] += 1
+        end
+        next if removed.include?(new_v)
 
-def find(result, buttons, raw)
-  variants = buttons.map { |b| b.tally }
-  i = 0
-  loop do
-    new_variants = Set.new
-    i += 1
-    variants.each do |v|
-      if result == v
-        puts "found #{i}: #{result} == #{v} -- #{raw}"
-        return i
-      end
-      next if v.any? { |key, _value| result[key] < v[key] } # we are bigger then needed already
+        if result.each_with_index.any? { |_k, i| new_v[i] > result[i] } # at least one value is already to high
+          removed << new_v
+          next
+        end
 
-      buttons.each do |b|
-        new_v = v.merge(b.tally) { |_, val1, val2| val1 + val2 }
-        new_variants << new_v
+        if values.include?(new_v) # we had the same state the round before, with less cost
+          removed << new_v
+          next
+        end
+        new_values << new_v
       end
     end
+    values = new_values
+  end
+  values
+end
+
+sum = 0
+data.each do |d|
+  variants = prefill(d[1], d[2], d[2].max, d[2].size)
+  puts "Build prefill for #{d[3]}, found #{variants.size}"
+  i = d[2].max
+  loop do
+    found = false
+    new_variants = Set.new
+    variants.each do |variant|
+      if variant == d[2]
+        puts "Found #{i} for #{d[3]}"
+        sum += i
+        found = true
+        break
+      end
+      d[1].each do |button|
+        new_variant = variant.dup
+        button.each do |digit|
+          new_variant[digit] += 1
+        end
+        new_variants << new_variant
+      end
+    end
+    break if found
+
+    i += 1
     variants = new_variants
   end
 end
 
-puts "Part 1: #{data.map { |d| find(d[2], d[1], d[3]) }.sum}"
+puts sum
